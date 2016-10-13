9f1db582e5e368f3b9b958339af0a9ee71b0aa13
Николай <gultuy00@gmail.com>
2016-10-13 19:12:02 +0300

    Добавлен проект консольного приложения

---
+++ Kontur.TestTask.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontur.TestTask.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
